e17631167ac7e0682c9c40fa2e69f35f69a85a95
Miguel Ortiz <Unknown>
2023-09-30 04:24:38 -0400

    updating file

--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 export const trackList = [
     {
       title: 'Smash it',
-      src: "assets/audio/smash-it.mp3",
+      src: "/assets/audio/SMASH IT!.mp3",
       author: 'Miguel Ortiz',
       liked:false
     },
     {
       title: 'beautiful presence-piano',
-      src: "assets/audio/beautiful-presence-piano.mp3",
+      src: "/assets/audio/beautiful-presence-piano.mp3",
       author: 'Miguel Ortiz',
       liked:false
     },
